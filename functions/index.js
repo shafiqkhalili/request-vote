174d6da2c768ba0454fa7f0730d613d6182c8761
Shafigh Khalili <shafiqkhalili@gmail.com>
2020-09-27 12:53:23 +0200

    Add API

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,12 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const express = require("express");
+const cors = require("cors");
+
 admin.initializeApp();
+
+const app = express();
+app.use(cors());
 
 // auth trigger (new user signup)
 exports.newUserSignUp = functions.auth.user().onCreate(user => {
@@ -85,3 +91,52 @@ exports.logActivities = functions.firestore.document('/{collection}/{id}')
         }
         return null;
     });
+
+//Express API
+
+app.get("/", async (req, res) => {
+    const snapshot = await admin.firestore().collection("users").get();
+
+    let users = [];
+    snapshot.forEach((doc) => {
+        let id = doc.id;
+        let data = doc.data();
+
+        users.push({ id, ...data });
+    });
+
+    res.status(200).send(JSON.stringify(users));
+});
+
+app.get("/:id", async (req, res) => {
+    const snapshot = await admin.firestore().collection('users').doc(req.params.id).get();
+
+    const userId = snapshot.id;
+    const userData = snapshot.data();
+
+    res.status(200).send(JSON.stringify({ id: userId, ...userData }));
+});
+
+app.post("/", async (req, res) => {
+    const user = req.body;
+
+    await admin.firestore().collection("users").add(user);
+
+    res.status(201).send();
+});
+
+app.put("/:id", async (req, res) => {
+    const body = req.body;
+
+    await admin.firestore().collection('users').doc(req.params.id).update(body);
+
+    res.status(200).send();
+});
+
+app.delete("/:id", async (req, res) => {
+    await admin.firestore().collection("users").doc(req.params.id).delete();
+
+    res.status(200).send();
+});
+
+exports.user = functions.https.onRequest(app);
